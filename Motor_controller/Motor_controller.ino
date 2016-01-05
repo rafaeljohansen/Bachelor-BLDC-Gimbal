e319096365f4b4b7626bfe64fbbe73c759a349e3
MCUdude <mcudude@users.noreply.github.com>
2016-01-06 00:01:00 +0100

    Fixed PCINT bug (motors kept interfering with each other)

--- Motor_controller/Motor_controller.ino
+++ Motor_controller/Motor_controller.ino
@@ -3,31 +3,33 @@
  trig a step on the motor on a rising edge.
 
        MOTOR1          MOTOR2
-Step:  A1/PC1          A2/PC2 
-Dir:   D2/PD2(/INT)    A3/PC3(/M_P)
+Step:  PD2/D2(/INT)    PC1/A1 
+Dir:   PC2/A2          PC3/A3(/M_P)
 */
 
 #include "BLDC.h"
 
+// Motors connected to pin 3,5,6 and 9,10,11
 BLDC Motor1(9,10,11);
 BLDC Motor2(3,5,6);
 
 
-
 void setup() 
 {
+  Serial.begin(9600);
   Motor1.begin();
   Motor2.begin();
 
-  DDRC &= ~0x0E; //0b00001110 -> set PC1, PC2 and PC3 as input
-  DDRD &= ~0x04; //0b00000100 -> set PD2 as input
-  PORTC |= 0x08; //0b00001000 -> enable input pullup for PC3
-  PORTD |= 0x04; //0b00000100 -> enable input pullup for PD2
+  DDRC &= ~0x0E; // 0b00001110 -> set PC1, PC2 and PC3 as input
+  DDRD &= ~0x04; // 0b00000100 -> set PD2 as input
+  PORTC |= 0x0E; // 0b00001110 -> enable input pullup for PC3
+  PORTD |= 0x04; // 0b00000100 -> enable input pullup for PD2
   
   // Pin Change Interrupt enable on PCINT1 vector (PCINT8 - PCINT14)
-  PCICR |= _BV(PCIE1);    // PCINT8 - 14 enable
-  PCMSK1 |= _BV(PCINT9);  // PCINT9 - PC1
-  PCMSK1 |= _BV(PCINT10); // PCINT10 - PC2
+  PCICR |= _BV(PCIE1);    // Enable PCIE1 (PCINT8 - 14)
+  PCICR |= _BV(PCIE2);    // Enable PCIE2 (PCINT16 - 23) 
+  PCMSK1 |= _BV(PCINT9);  // Enable PCINT9 (PC1)
+  PCMSK2 |= _BV(PCINT18); // Enable PCINT18 (PD2)
 
   sei(); // Turn global interrupt on.
 }
@@ -36,23 +38,30 @@ void setup()
 
 void loop() 
 {
-  while(1); //let the interrupt handle everything
+  while(1); // Let the interrupt handle everything
 }
 
 
 
-ISR(PCINT1_vect) //PCINT8 - 14
+// Trigs when PC1 (PCINT9) detects a rising edge
+ISR(PCINT1_vect) 
 { 
-  if(PINC & _BV(PC1)) //PCINT9 trig on positive edge
-  { 
-    ((PIND & 0x04) ? Motor1.step(true) : Motor1.step(false));
-   // If PD2 = HIGH -> Motor1.step(true), else -> Motor1.step(false)
-  }
-
-  if(PINC & _BV(PC2)) //PCINT10 trig on positive edge
-  {
-    ((PINC & 0x08) ? Motor2.step(true) : Motor2.step(false));
-    // If PC3 = HIGH -> Motor2.step(true), else -> Motor2.step(false)
-  }
-   
+  if(PINC & _BV(PC1)) // if(PC1.read == true)
+  {   
+    ((PINC & _BV(PC3)) ? Motor2.step(true) : Motor2.step(false));
+    // If PC3 == HIGH -> Motor2.step(true), else -> Motor2.step(false)
+  }  
 }
+
+
+
+// Trigs when PD2 (PCINT18) detects a rising edge
+ISR(PCINT2_vect) 
+{
+  if(PIND & _BV(PD2)) // if(PD2.read == true)
+  {    
+    ((PINC & _BV(PC2)) ? Motor1.step(true) : Motor1.step(false));
+   // If PC2 == HIGH -> Motor1.step(true), else -> Motor1.step(false)
+  }
+}
+
