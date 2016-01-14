430b2cff69b8dd6c600b2ae42544dd0a581a4b1d
MCUdude <mcudude@users.noreply.github.com>
2016-01-14 14:28:34 +0100

    millis to micros

--- Timer/examples/blink2/blink2.ino
+++ Timer/examples/blink2/blink2.ino
@@ -8,11 +8,11 @@
 
 #include "Timer.h"                     //http://github.com/JChristensen/Timer
 
-const int LED1 = 8;                    //connect one LED to this pin (with appropriate current-limiting resistor of course)
-const int LED2 = 9;                    //connect another LED to this pin (don't forget the resistor)
-const unsigned long PERIOD1 = 1000;    //one second
-const unsigned long PERIOD2 = 10000;   //ten seconds
-Timer t;                               //instantiate the timer object
+const int LED1 = 12;                    //connect one LED to this pin (with appropriate current-limiting resistor of course)
+const int LED2 = 13;                    //connect another LED to this pin (don't forget the resistor)
+const unsigned long PERIOD1 = 100000;   // 1/10th second
+const unsigned long PERIOD2 = 1000000;  //One second (1 * 1000 * 1000)
+Timer t;                                //instantiate the timer object
 
 void setup(void)
 {
