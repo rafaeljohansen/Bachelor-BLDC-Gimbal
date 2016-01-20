b02ef015181f13a0d672b84b4f1955c0b6cb5bfa
MCUdude <mcudude@users.noreply.github.com>
2016-01-20 14:52:47 +0100

    changes

--- Main_project/Main_project.ino
+++ Main_project/Main_project.ino
@@ -3,42 +3,61 @@
 //<Wire.h>
 //PID.h
 //Blynk
-//"Timer.h"
+#include "Timer.h"
 
 
 /***** Pin definitions *****/
 volatile int altitudeInterrupt = 5;
-volatile int azimuthInterrupt = 4;
+//volatile int azimuthInterrupt = 4;
 
 
 /***** Variable declarations *****/
-
+uint32_t counter;
 
 
 /***** Objects *****/
 Remote_control Angle(altitudeInterrupt);
-Remote_control Azimuth(azimuthInterrupt);
-
+//Remote_control Azimuth(azimuthInterrupt);
+Timer t;
 
 
 
 void setup() 
 {
-  Serial.begin(115200);
+  Serial.begin(250000);
+  pinMode(4, OUTPUT);
   Angle.begin();
-  Azimuth.begin();
-
+//  Azimuth.begin();
+  t.every(10, step, (void*)0);
 }
 
 
 
 void loop() 
 {
-    delay(100);
-    Serial.println(Azimuth.pulseWidth);
-    Serial.println(Angle.pulseWidth);
+  t.update();
+ // delay(100);
+ // Serial.println(Azimuth.pulseWidth);
+ // Serial.println(Angle.pulseWidth);
+  yield();
 }
 
+void step(void* context)
+{
+  uint32_t pulse = Angle.pulseWidth;
+  counter++;
+  pulse = map(pulse, 1000, 1950, 1000, 10);
+
+  //(pulse > 1850) ? pulse = 4294967295 : 0 ;
+ 
+  if((counter % pulse) == 0)
+  {
+    digitalWrite(4, HIGH);
+    delayMicroseconds(8);
+    digitalWrite(4, LOW);
+  }
+  
+}
 
 
 
