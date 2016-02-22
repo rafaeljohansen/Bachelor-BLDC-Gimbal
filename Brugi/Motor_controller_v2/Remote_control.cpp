58f8c70288129626adedc56861f6418006c94f50
rafaeljohansen <rafael.johansen@hotmail.com>
2016-02-22 10:47:26 +0100

    Blynk implementation

--- Brugi/Motor_controller_v2/Remote_control.cpp
+++ Brugi/Motor_controller_v2/Remote_control.cpp
@@ -18,24 +18,24 @@ void Remote_control::readRemote(uint64_t currentMicros)
 
 
   /****** If remote is enabled ******/
-    if(!(PINC & _BV(PC3))) 
+    if(!(PINC & _BV(PC3))) // 00001111 & 00001000 = 00001000 -> 11110111
     {
       if(dutyCycle > 50) // Dutycycle more than 50
       {
-        m_CWinterval = map(dutyCycle, 62, 51,10000,40000);
-        m_CCWinterval = UINT32_MAX;
+        m_CWintervalRC = map(dutyCycle, 62, 51,10000,40000);
+        m_CCWintervalRC = UINT32_MAX;
       }
       
       else if(dutyCycle < 50) // Dutycycle less than 50
       {
-        m_CWinterval = UINT32_MAX;
-        m_CCWinterval = map(dutyCycle, 38, 49,10000,40000);
+        m_CWintervalRC = UINT32_MAX;
+        m_CCWintervalRC = map(dutyCycle, 38, 49,10000,40000);
       }
 
       else // Dutycycle equals 50
       {
-        m_CWinterval = UINT32_MAX; // Freeze clock wise
-        m_CCWinterval = UINT32_MAX; // Freeze counter clock wise
+        m_CWintervalRC = UINT32_MAX; // Freeze clock wise
+        m_CCWintervalRC = UINT32_MAX; // Freeze counter clock wise
       }
     }
   /**********************************/
@@ -44,8 +44,8 @@ void Remote_control::readRemote(uint64_t currentMicros)
   /****** Else if remote is disabled ******/
     else
     {
-      m_CWinterval = UINT32_MAX; // Freeze clock wise
-      m_CCWinterval = UINT32_MAX; // Freeze counter clock wise
+      m_CWintervalRC = UINT32_MAX; // Freeze clock wise
+      m_CCWintervalRC = UINT32_MAX; // Freeze counter clock wise
     }
   /****************************************/    
 
@@ -53,22 +53,41 @@ void Remote_control::readRemote(uint64_t currentMicros)
   }
 }
 
+void Remote_control::readBlynk(uint64_t currentMicros)
+{
+  
+}
 
 
 int16_t Remote_control::setPoint(uint64_t currentMicros)
 {
-  if(currentMicros - previousMicrosSetpoint >= m_CWinterval)
+  /****** RC changing setpoint ******/
+  if(currentMicros - previousMicrosSetpoint >= m_CWintervalRC)
   {
     previousMicrosSetpoint = currentMicros;
     currentSetPoint++;
   }
 
-  if(currentMicros - previousMicrosSetpoint >= m_CCWinterval)
+  if(currentMicros - previousMicrosSetpoint >= m_CCWintervalRC)
   {
     previousMicrosSetpoint = currentMicros;
     currentSetPoint--;
   }
-    
+  /**********************************/
+  
+  /****** Blynk changing setpoint ******/
+  if(currentMicros - previousMicrosSetpoint >= m_CWintervalBlynk)
+  {
+    previousMicrosSetpoint = currentMicros;
+    currentSetPoint++;
+  }
+
+  if(currentMicros - previousMicrosSetpoint >= m_CCWintervalBlynk)
+  {
+    previousMicrosSetpoint = currentMicros;
+    currentSetPoint--;
+  }
+  /*************************************/   
   return currentSetPoint;
 }
 
